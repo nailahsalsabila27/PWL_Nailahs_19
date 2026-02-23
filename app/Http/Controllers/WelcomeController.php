2c2e7a81ec0bacae6c47509e77f2461a4562c667
nailahsalsabila27 <nailahslsb@gmail.com>
2026-02-23 12:20:08 +0700

    Jobsheet1_Praktikum3

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -6,7 +6,13 @@ use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
 {
-    public function hello() {
-        return 'Hello World';
+    public function hello(){
+        return('Hello World');
+    }
+
+public function greeting(){
+        return view('blog.hello')
+            ->with('name','Nailsah Salsabila Maulidiyah')
+            ->with('occupation','Astronaut');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -68,4 +68,15 @@ Route::resource('photos', PhotoController::class)->except([
     'create', 'store', 'update', 'destroy'
 ]);
 
+// Route::get('/greeting', function () {
+// 	return view('hello', ['name' => 'Nailah']);
+// });
+
+// Route::get('/greeting', function () {
+// 	return view('blog.hello', ['name' => 'Nailah']);
+// });
+
+use App\Http\Controllers\WelcomeController;
+Route::get('/greeting', [WelcomeController::class, 'greeting']);
+
 
