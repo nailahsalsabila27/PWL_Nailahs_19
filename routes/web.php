d0670d969c920c38ce56019efdb5360dfdc122c5
nailahsalsabila27 <nailahslsb@gmail.com>
2026-02-23 11:51:56 +0700

    Jobsheet1_praktiku2b

--- routes/web.php
+++ routes/web.php
@@ -54,3 +54,18 @@ Route::get('/about', AboutController::class);
 
 use App\Http\Controllers\ArticleController;
 Route::get('/articles/{id}', ArticleController::class);
+
+
+use App\Http\Controllers\PhotoController;
+
+Route::resource('photos', PhotoController::class);
+
+Route::resource('photos', PhotoController::class)->only([
+    'index', 'show'
+]);
+
+Route::resource('photos', PhotoController::class)->except([
+    'create', 'store', 'update', 'destroy'
+]);
+
+
